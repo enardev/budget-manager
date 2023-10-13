48ccfa92013dbd8746aea55e797e5cd46c36fbff
Enaldo Narvaez Yepes <enaldo_17@hotmail.com>
2023-10-12 22:03:10 -0500

    feat: add application configuration

--- app/src/app/app.go
+++ app/src/app/app.go
@@ -1,17 +1,41 @@
 package main
 
 import (
-	"net/http"
+	"log"
 
-	"github.com/gin-gonic/gin"
+	configv2 "github.com/gookit/config/v2"
+
+	"github.com/enaldo1709/budget-manager/domain/usecase/src/usecase"
+	"github.com/enaldo1709/budget-manager/infrastructure/adapters/postgresql-adapter/src/postgresql"
+	"github.com/enaldo1709/budget-manager/infrastructure/adapters/postgresql-adapter/src/postgresql/postgresconfig"
+	"github.com/enaldo1709/budget-manager/infrastructure/entry-points/web-api/src/api"
+	"github.com/enaldo1709/budget-manager/infrastructure/helpers/configutil/src/configutil"
 )
 
 func main() {
-	app := gin.Default()
+	configutil.LoadConfig()
 
-	app.GET("/health", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{"status": "UP"})
-	})
+	// PostgreSQL database configuration
+	properties := postgresconfig.PostgreSqlConnectionProperties{}
+	configv2.MapStruct("db.properties", &properties)
 
-	app.Run()
+	//repository := postgresql.NewExpensePostgresAdapter(properties, db)
+	db := postgresconfig.CreateSqlConnection(properties)
+	repository := postgresql.NewExpensePostgresAdapter(properties, db)
+
+	defer func() {
+		if err := db.Close(); err != nil {
+			log.Fatal("error: error closing database connection...", err)
+		}
+	}()
+
+	// UseCase configuration
+	expenseUseCase := usecase.ExpenseUseCase{Repository: repository}
+
+	// Api configuration
+	budgetHandler := api.BudgetHandler{
+		ExpenseUseCase: expenseUseCase,
+	}
+	router := api.ConfigRouter(budgetHandler)
+	router.Run()
 }
